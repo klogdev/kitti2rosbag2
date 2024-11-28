1b004d69bf837c4e97fb69e0369800dc7e58e08e
klogdev <klog.dev.team@gmail.com>
2024-11-28 02:52:39 +0000

    Add id= to TopicMetadata, and add two new topics for testing.

--- kitti2rosbag2/kitti_rec.py
+++ kitti2rosbag2/kitti_rec.py
@@ -7,6 +7,8 @@ import rosbag2_py
 import os
 from rclpy.node import Node
 from sensor_msgs.msg import Image, CameraInfo
+from geometry_msgs.msg import QuaternionStamped, Vector3Stamped
+from builtin_interfaces.msg import Time
 from cv_bridge import CvBridge
 from nav_msgs.msg import Odometry, Path
 from geometry_msgs.msg import TransformStamped, PoseStamped
@@ -68,12 +70,15 @@ class Kitti_Odom(Node):
             converter_options = rosbag2_py._storage.ConverterOptions('', '')
             self.writer.open(storage_options, converter_options)
 
-        left_img_topic_info = rosbag2_py._storage.TopicMetadata(name='/camera2/left/image_raw', type='sensor_msgs/msg/Image', serialization_format='cdr')
-        right_img_topic_info = rosbag2_py._storage.TopicMetadata(name='/camera3/right/image_raw', type='sensor_msgs/msg/Image', serialization_format='cdr')
-        odom_topic_info = rosbag2_py._storage.TopicMetadata(name='/car/base/odom', type='nav_msgs/msg/Odometry', serialization_format='cdr')
-        path_topic_info = rosbag2_py._storage.TopicMetadata(name='/car/base/odom_path', type='nav_msgs/msg/Path', serialization_format='cdr')
-        left_cam_topic_info = rosbag2_py._storage.TopicMetadata(name='/camera2/left/camera_info', type='sensor_msgs/msg/CameraInfo', serialization_format='cdr')
-        right_cam_topic_info = rosbag2_py._storage.TopicMetadata(name='/camera3/right/camera_info', type='sensor_msgs/msg/CameraInfo', serialization_format='cdr')
+        left_img_topic_info = rosbag2_py._storage.TopicMetadata(id=0, name='/camera2/left/image_raw', type='sensor_msgs/msg/Image', serialization_format='cdr')
+        right_img_topic_info = rosbag2_py._storage.TopicMetadata(id=1, name='/camera3/right/image_raw', type='sensor_msgs/msg/Image', serialization_format='cdr')
+        odom_topic_info = rosbag2_py._storage.TopicMetadata(id=2, name='/car/base/odom', type='nav_msgs/msg/Odometry', serialization_format='cdr')
+        path_topic_info = rosbag2_py._storage.TopicMetadata(id=3, name='/car/base/odom_path', type='nav_msgs/msg/Path', serialization_format='cdr')
+        left_cam_topic_info = rosbag2_py._storage.TopicMetadata(id=4, name='/camera2/left/camera_info', type='sensor_msgs/msg/CameraInfo', serialization_format='cdr')
+        right_cam_topic_info = rosbag2_py._storage.TopicMetadata(id=5, name='/camera3/right/camera_info', type='sensor_msgs/msg/CameraInfo', serialization_format='cdr')
+        right_cam_topic_info = rosbag2_py._storage.TopicMetadata(id=5, name='/camera3/right/camera_info', type='sensor_msgs/msg/CameraInfo', serialization_format='cdr')
+        pose_rotation_stamped = rosbag2_py._storage.TopicMetadata(id=6, name='/pose/base/rotation_stamped', type='geometry_msgs/msg/QuaternionStamped', serialization_format='cdr')
+        pose_translation_stamped = rosbag2_py._storage.TopicMetadata(id=7, name='/pose/base/translation_stamped', type='geometry_msgs/msg/Vector3Stamped', serialization_format='cdr')
 
         self.writer.create_topic(left_img_topic_info)
         self.writer.create_topic(right_img_topic_info)
@@ -81,6 +86,8 @@ class Kitti_Odom(Node):
         self.writer.create_topic(path_topic_info)
         self.writer.create_topic(left_cam_topic_info)
         self.writer.create_topic(right_cam_topic_info)
+        self.writer.create_topic(pose_rotation_stamped)
+        self.writer.create_topic(pose_translation_stamped)
 
         # path msg
         self.p_msg = Path()
@@ -112,8 +119,9 @@ class Kitti_Odom(Node):
             quaternion = Quaternion()
             quaternion = quaternion.rotationmtx_to_quaternion(self.ground_truth[self.counter][:3, :3])
             self.rec_odom_msg(translation, quaternion, timestamp_ns)
+            self.rec_odom_msg2(translation, quaternion, timestamp_ns)
 
-        self.get_logger().info(f'{self.counter}-Images Processed')
+        self.get_logger().info(f'{self.counter}-Images Processed, on timestamp_ns {timestamp_ns}')
         if self.counter >= self.counter_limit:
             self.get_logger().info('All images and poses published. Stopping...')
             rclpy.shutdown()
@@ -142,6 +150,27 @@ class Kitti_Odom(Node):
         self.writer.write('/car/base/odom', serialize_message(odom_msg), timestamp)
         self.rec_path_msg(odom_msg, timestamp)
         return
+
+    def rec_odom_msg2(self, translation, quaternion, timestamp_ns):
+        rotation_stamped = QuaternionStamped()
+        translation_stamped = Vector3Stamped()
+
+        translation_stamped.header.frame_id = "map"
+        translation_stamped.header.stamp  = self.ns_to_time(timestamp_ns)
+        # self.get_logger().info(f'head-time secs {translation_stamped.header.stamp.sec} nsec {translation_stamped.header.stamp.nanosec}')
+        translation_stamped.vector.z = -translation[1]
+        translation_stamped.vector.y = -translation[2] 
+        translation_stamped.vector.x = -translation[0]
+        self.writer.write('/pose/base/translation_stamped', serialize_message(translation_stamped), timestamp_ns)
+
+        rotation_stamped.header.frame_id = "map"
+        rotation_stamped.header.stamp  = self.ns_to_time(timestamp_ns)
+        rotation_stamped.quaternion.x = quaternion[0]
+        rotation_stamped.quaternion.y = quaternion[1]
+        rotation_stamped.quaternion.z = quaternion[2]
+        rotation_stamped.quaternion.w = quaternion[3]
+        self.writer.write('/pose/base/rotation_stamped', serialize_message(rotation_stamped), timestamp_ns)
+        return
     
     def rec_path_msg(self, odom_msg, timestamp):
         pose= PoseStamped()
@@ -151,6 +180,14 @@ class Kitti_Odom(Node):
         self.p_msg.header.frame_id = "map"
         self.writer.write('/car/base/odom_path', serialize_message(self.p_msg), timestamp)
         return
+
+    @staticmethod
+    def ns_to_time(timestamp_ns):
+        # Convert nanoseconds to a Time message
+        time_msg = Time()
+        time_msg.sec = timestamp_ns // 1_000_000_000  # Seconds part
+        time_msg.nanosec = timestamp_ns % 1_000_000_000  # Nanoseconds part
+        return time_msg
 
 def main(args=None):
     rclpy.init(args=args)
